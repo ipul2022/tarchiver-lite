7e70143f03277d6dd8a77bb4d80b55a4d1de783b
HeiByte <HeiByte@users.noreply.github.com>
2026-04-16 19:19:22 +0700

    update script.js dan styles.css

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 const hamMenu = document.querySelector(".hamburger-menu");
 const mobileMenu = document.querySelector("ul.mobile-menu");
+const btnClsMobile = document.querySelector("nav ul.mobile-menu .close-btn");
 
+
+//Menu Hamburger saat di klik
 hamMenu.addEventListener("click", () => {
   if (hamMenu.classList.contains("is-active")) {
     hamMenu.classList.remove("is-active");
@@ -10,3 +13,9 @@ hamMenu.addEventListener("click", () => {
     mobileMenu.classList.add("is-open");
   }
 });
+
+//Menu Close pada mobile
+btnClsMobile.addEventListener("click", () => {
+  hamMenu.classList.remove("is-active");
+  mobileMenu.classList.remove("is-open");
+})
